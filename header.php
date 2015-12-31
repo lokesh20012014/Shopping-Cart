1c3a90b8dd6b89e5c2e4b6270247ae9f3b88b2b9
Lokesh Kumar J R <lokesh.kumar20012012@live.com>
2015-12-31 15:53:53 +0100

    Made some changes to header file

--- header.php
+++ header.php
@@ -1,27 +1,44 @@
+
+
+
 <?php
 session_start();
-if(isset($_SESSION['SESS_CHANGEID']) == TRUE) {
+if(isset($_SESSION['SESS_CHANGEID']) == TRUE) // a check is made for a session variable called SESS_CHANGEID. If it exists, the session is unset (deleted) and the session id is regenerated.
+{
 session_unset();
 session_regenerate_id();
 }
-require("config.php");
+require("connect.php");
+
 ?>
-<?php echo $config_sitename; ?>
-<div id="header"></div>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<head>
+<title><?php echo $config_sitename; ?></title>
+
+<link href="stylesheet.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="header">
+<h1><?php echo $config_sitename; ?></h1>
+</div>
 <div id="menu">
 <a href="<?php echo $config_basedir; ?>">Home</a> -
-<a href="<?php echo $config_basedir;?>showcart.php">View Basket/Checkout</a></div>
+<a href="<?php echo $config_basedir;?>showcart.php">View Basket/Checkout</a>
+</div>
 <div id="container">
 <div id="bar">
 <?php
 require("bar.php");
-echo "<hr />";
-if(isset($_SESSION['SESS_LOGGEDIN']) == TRUE)
+echo "<hr>";
+if(isset($_SESSION['SESS_LOGGEDIN']))  //  A check is made to see if the SESS_LOGGEDIN variable exists. If it does, it indicates the user is currently logged in, and his username and a Logout link are displayed.
 {
-echo "Logged in as <strong>" . $_SESSION['SESS_USERNAME']. "</strong>[<a href="&quot; . $config_basedir. &quot;logout.php">logout</a>]";
+echo "Logged in as <strong>" . $_SESSION['SESS_USERNAME']. "</strong>[<a href='" . $config_basedir. "logout.php'>logout</a>]";  // The username is stored in SESS_USERNAME; this variable is created in login.php
 }
-else{
-echo "<a href="&quot;. $config_basedir . &quot;login.php">Login</a>";
+else
+{
+echo "<a href='". $config_basedir . "login.php'>Login</a>";
 }
-?></div>
+?>
+</div>
 <div id="main">
