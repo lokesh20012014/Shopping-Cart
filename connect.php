3357ace2c4c5d9cecccba6743bc7ed134da61c23
Lokesh Kumar J R <lokesh.kumar20012012@live.com>
2015-12-31 15:53:06 +0100

    Changes to Database

--- connect.php
+++ connect.php
@@ -3,9 +3,8 @@ $user = 'root';
 $password = 'root';
 $db = 'shopping';
 $host = 'localhost';
+$config_basedir = "http://localhost:8888/Shopping-Cart/";
+$config_sitename = "shopping";
 $port = 8889;
 $success = mysqli_connect($host, $user, $password, $db, $port);
 ?>
-
-
-
